794f316d407f485e58e7b55088e7070dbac28bbc
alatar- <alatar@alatar.eu>
2013-04-09 13:44:41 +0200

    created setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+PACKAGE_DIR = 'src'
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+setup(
+    name = "pytenseshift",
+    version = '0.0.1',
+    author = read("AUTHORS"),
+    url = "https://github.com/perfidia/pytenseshift",
+    package_dir = {'': PACKAGE_DIR},
+    long_description = read("README.md"),
+    packages = find_packages(PACKAGE_DIR, exclude=['ez_setup', 'examples', 'tests']),
+    zip_safe = False,
+    install_requires = [
+        'AnDiP',
+        'nltk'
+        ]
+    )
