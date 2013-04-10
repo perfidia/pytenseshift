9da04e18d6a485449d17bc5a5fb8c45d1f9fa202
alatar- <alatar@alatar.eu>
2013-04-10 19:23:11 +0200

    EN: added few examples

--- src/main.py
+++ src/main.py
@@ -8,7 +8,53 @@ from pytenseshift import PlPyTenseShift
 from pytenseshift import EnPyTenseShift
 
 # pl = PlPyTenseShift()
-en = EnPyTenseShift()
-
 # print pl.getPastTense("Jadę na rowerze.")
-print en.getPastTense("I am big enthusiast of Python language.")
+
+
+import en
+
+print en.verb.past("will")
+print en.verb.past("does")
+print en.verb.past("can")
+# print en.verb.past("may")
+# print en.verb.past("must")
+print en.verb.past("have")
+# print en.verb.past("ought")
+
+#
+# print en.verb.past("is", negate=True)
+# print en.verb.past("are", person=1, negate=False)
+# print en.verb.past("are", person=3, negate=False)
+# print en.verb.past("are", person=2, negate=False)
+# print en.verb.past("are")
+#
+# for i in ['swimming', 'making', 'am', 'are', 'fighting', 'was']:
+#     # print i, en.is_verb(i), en.verb.infinitive(i), en.verb.present(i, person=1, negate=True), en.verb.past(i, person="3rd", negate=False)
+#     print en.verb.conjugate(i, "past", negate=False), en.verb.is_tense(i, "1st singular past", negated=False)
+#     # are 1, 2, 3, "1st", "2nd", "3rd", "plural", "*". Just use the one you like most.
+
+# print en.verb.present("gave", person=3, negate=False)
+
+
+
+en = EnPyTenseShift()
+print en.getPastTense("I have to do it.")
+# print en.getPastTense("I am big enthusiast of Python language.")
+# print en.getPastTense("I go out door and play football.")
+# print en.getPastTense("I go to visit my parents.")
+# print en.getPastTense("Ther aren't crazy. They are pretty awesome!")
+# print en.getPastTense("We are making photos and creating a lot of modern realistic landscapes.")
+# print en.getPastTense("You can use the library to conjugate verbs, pluralize nouns, write out numbers, find dictionary descriptions and synonyms for words, summarise texts and parse grammatical structure from sentences.")
+# print en.getPastTense("It can allow to do it, making sandwich.")
+# print en.getPastTense("There's not need to develop the system which will allow collecting and presenting information which are important from the KNOW criteria’s point of view.")
+# print en.getPastTense("He does the washing every weekend while I don't do it anytime.")
+# print en.getPastTense("Mo girlfriend said that the Lord of the Rings movies are boring. Now, i'm a single.")
+# print en.getPastTense("The world is not in your books and maps. It's out there.")
+# print en.getPastTense(''' Home is behind, the world ahead,
+# And there are many paths to tread
+# Through shadows to the edge of night,
+# Until the stars are all alight.
+# Then world behind and home ahead,
+# We'll wander back and home to bed.
+# Mist and twilight, cloud and shade,
+# Away shall fade! Away shall fade! ''')
--- src/pytenseshift/__init__.py
+++ src/pytenseshift/__init__.py
@@ -2,18 +2,20 @@
 from django.conf.locale import te
 
 from nltk.corpus import pl196x, treebank
-from nltk.tag import UnigramTagger
+from nltk.tag import UnigramTagger, DefaultTagger
 from nltk.tokenize import word_tokenize as tokenize
-
+import en
 # from andip import AnDiP
 # from andip.provider import FileProvider, DatabaseProvider, PlWikiProvider
 
 class PyTenseShift():
 
     def __init__(self, corpus):
-        self.__utag = UnigramTagger(corpus.tagged_sents())
+        dtag = DefaultTagger("NN")
+        self.__utag = UnigramTagger(corpus.tagged_sents(), backoff = dtag)
 
     def _tokenize(self, tense):
+
         return self.__utag.tag(tokenize(tense))
 
     def getPastTense(self, tense):
@@ -42,9 +44,31 @@ class EnPyTenseShift(PyTenseShift):
 
     def __init__(self):
         PyTenseShift.__init__(self, treebank)
+        self.__en = en
 
     def getPastTense(self, tense):
 
         words = self._tokenize(tense)
 
-        return words
+
+        for (word, form) in words:
+            if form[0:2] == 'VB' : # VBP VBZ MD
+                # try:
+                #     # en.verb.past(word)
+                #     # en.verb.conjugate()
+                # except KeyError:
+                #     pass
+                # else:
+                #     pass
+                print word, form, self.__en.is_verb(word)
+
+                # TODO
+                # czasownik po czasowniku - drugi ignored "will not do"
+                # czasownik z "to"
+                # modale ręcznie
+                # nieregularny być - wtedy trzeba
+                # przeczenia 't lub not, nt?
+
+                # formy skrótowe 'm 're 's 'll
+
+        # return words
